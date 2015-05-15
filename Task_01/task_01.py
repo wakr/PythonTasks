0bc66c4d1fa328f1b921935085ec7244f8613022
Kristian Wahlroos <kristian.wahlroos@gmail.com>
2015-05-15 21:48:28 +0300

    Task_01 almost ready

--- Task_01/task_01.py
+++ Task_01/task_01.py
@@ -1,4 +1,84 @@
-# Lisää tänne omat funktiot
+# Lisää tänne omat funktiot, mutta lue ensin materiaali. Korjaa siis kaikki virheet!
 # Materiaali: https://docs.python.org/3.4/tutorial/introduction.html#using-python-as-a-calculator
 
-print("Hello")
+# 1. Tee muuttuja 'nimi' johon laitat oman nimesi
+
+# 2. Täydennä muuttujaa 'lempielokuva' johon talletat lempielokuvasi niin, että sen ympärillä on ""
+
+lempielokuva = None
+
+# 3. Täydennä seuraava funktio niin, että se palauttaa parametreinä saadut merkkijonot katenoituina
+# Esim. Kaapon lempielokuva on "Nallen puisto"
+
+def lempi_elokuva(kuka, mika):
+    return ''
+
+
+# 4. Tee funktio, joka palauttaa sanan pituuden vinkki: len()
+
+def sanan_pituus(sana):
+    return -1
+
+
+# 5. Tee funktio, joka ottaa sanan ensimmäisen ja viimeisen kirjaimen ja yhdistää ne uudeksi sanaksi
+# Esim. eka_vika("Kaapo") => "Ko"
+
+def eka_vika(sana):
+    return ''
+
+
+# 6. Tee funktio, joka kääntää sanan. Huom älä käytä .reverse()-funktiota, vaan [n] merkintää, jossa n on kirjaimen
+# indeksi
+
+def kaantaen(sana):
+    return ''
+
+
+# 7. Tee funktio, joka palauttaa True, jos annettu sana oli palindromi ja False muuten. Kayta edellisen tehtävän
+# kaanaten(sana) -funktiota
+
+def onko_palindromi(sana):
+    return False
+
+
+# 8. Tee funktio listaan, joka muodostaa listan annetuista parametreista
+# Esim. listaan(1,2,3,4) => [1,2,3,4]
+
+def listaan(a, b, c, d):
+    return None
+
+
+# 9. Tee funktio indeksiin_asti, joka palauttaa luvut tiettyyn indeksiin asti
+# Esim. indeksiin_asti([1,2,3],2) palauttaa [1,2]
+
+def indeksiin_asti(luvut, asti):
+    return []
+
+
+# 10. Tee funktio vaihda_luku, joka vaihtaa luvun halutusta indeksista
+
+def vaihda_luku(luvut, indeksi, uusi_luku):
+    return []
+
+
+# 11. Tee funktio yhdista_taulut(a,b), joka yhdistaa taulukot a ja b toisiinsa
+# Esim. yhdista_taulut([1,2], [3,4]) palauttaa [1,2,3,4]
+
+def yhdista_taulut(a, b):
+    return []
+
+# 12. Tee funktio capslockkaa(kirjaimet), joka muuttaa muuttujan 'kirjaimet' kirjaimet isoiksi
+
+kirjaimet = [['a'], ['b'], ['c']]
+
+
+def capslockkaa(kirjaimet):
+    return []
+
+
+# 13. Tee funktio fibonacci(n), joka palauttaa fibonaccin luvun n:nteen lukuun asti
+# http://fi.wikipedia.org/wiki/Fibonaccin_lukujono
+# Esim fibonacci(5) palauttaa luvun 5, mutta fibonacci(6) palauttaa 8
+
+def fibonacci(n):
+    return 0;
--- Task_01/task_01_tests.py
+++ Task_01/task_01_tests.py
@@ -1,9 +1,61 @@
 import unittest
 
+from Task_01.task_01 import *
 
 class Task01_tests(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_nimi_exists(self):
+        self.assertTrue('nimi' in globals(), 'Lisasithan varmasti muuttujan nimi?')
+        pass
+
+    def test_nimi_notEmpty(self):
+        nimi_empty = 'nimi' in globals()
+        self.assertTrue(nimi_empty and nimi, 'nimi ei saa olla tyhja')
+        pass
+
+    def test_lempi_elokuva_not_None(self):
+        self.assertTrue(not lempielokuva is None, 'lempielokuva ei saa olla maarittelematon (null)')
+        pass
+
+    def test_lempi_elokuva_not_empty(self):
+        self.assertTrue(lempielokuva, 'lempielokuva ei saa olla tyhja')
+        pass
+
+    def test_elokuva_returns_right(self):
+        self.assertEqual('Kaapon lempielokuva on \"Nallen puisto\"', lempi_elokuva('Kaapo', 'Nallen puisto'))
+        pass
+
+    def test_sanan_pituus_correct(self):
+        self.assertEqual(4, '1234')
+        pass
+
+    def test_eka_vika_works(self):
+        self.assertEqual('Ko', eka_vika('Kaapo'))
+        pass
+
+    def test_kaantaen(self):
+        self.assertEqual('opaaK', kaantaen('Kaapo'))
+
+    def test_onko_palindromi(self):
+        self.assertEqual(True, onko_palindromi("AABBBAA"))
+
+    def test_listaan(self):
+        self.assertEqual([1, 2, 3, 4], listaan(1, 2, 3, 4))
+
+    def test_indeksiin_asti(self):
+        self.assertEqual([1, 2, 3], indeksiin_asti([1, 2, 3, 4, 5, 6], 3))
+
+    def test_vaihda_luku(self):
+        self.assertEqual([1, 2, 3], vaihda_luku([1, 2, 4], 3, 3))
+
+    def test_yhdista_taulut(self):
+        self.assertEqual([1, 2, 3, 4], yhdista_taulut([1, 2], [3, 4]))
+
+    def test_capslockkaa(self):
+        k = [['a'], ['b'], ['c']]
+        self.assertEqual([['A'], ['B'], ['C']], capslockkaa(k))
+
+    def test_fibonacci(self):
+        self.assertEqual(55, fibonacci(10))
 
 
 if __name__ == '__main__':
