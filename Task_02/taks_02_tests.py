250380059fa846a12403142ed441e75ff0f84401
Kristian Wahlroos <kristian.wahlroos@gmail.com>
2015-05-22 20:19:22 +0300

    Task_01 ready

--- Task_02/taks_02_tests.py
+++ Task_02/taks_02_tests.py
@@ -1,11 +1,39 @@
-__author__ = 'Kr1d3'
-
 import unittest
+
+import Task_02.task_02 as Task02
+from Task_02.task_02 import *
+
+
+def moc_input(s):
+    return 11
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def setUp(self):
+        Task02.input = moc_input
+
+    def test_kerro_arvo(self):
+        self.assertEqual("jeejee", kerro_arvo())
+
+    def test_fizz_buzz(self):
+        self.assertEqual("Fizz", fizz_buzz(6))
+        self.assertEqual("Buzz", fizz_buzz(50))
+        self.assertEqual("FizzBuzz", fizz_buzz(30))
+
+    def test_suuret_alkuun(self):
+        self.assertEqual([10, 8, 6, 4, 2, 1, 3, 5, 7, 9], suuret_alkuun())
+
+    def test_pisin_sanat(self):
+        self.assertEqual("Kaapon kesavene", pisin_sana(["Auto", "Makkaru", "Pekan kesavene", "Kaapon kesavene"]))
+        self.assertEqual("Makkaruuuuuuuuuu",
+                         pisin_sana(["Auto", "Makkaruuuuuuuuuu", "Pekan kesavene", "Kaapon kesavene"]))
+
+    def test_pisimman_indeksi(self):
+        self.assertEqual(("AA", 0), pisimman_indeksi(["AA", "B", "C"]))
+        self.assertEqual(("AA", 2), pisimman_indeksi(["AA", "BBBB", "CCCCC"]))
+
+
+
 
 
 if __name__ == '__main__':
--- Task_02/task_02.py
+++ Task_02/task_02.py
@@ -1,1 +1,41 @@
-__author__ = 'Kr1d3'
+# 1. Tee funktio kerro_arvo() joka kysyy numeron kayttajalta ja palauttaa seuraavat merkkijonot väleiltä
+#  1-10 => "jee"
+#  11 => "jeejee"
+#  Negatiivisille => "Kaapon vene"
+#  Lopuille => "Maarittelematon"
+
+def kerro_arvo():
+    return ""
+
+
+# 2. Legendaarinen FizzBuzz http://blog.codinghorror.com/why-cant-programmers-program/
+# Tee ohjelma fizz_buzz(numero), joka palauttaa "Fizz" jos numero oli jaollinen kolmella, "Buzz" jos
+# numero oli jaollinen 5 ja "FizzBuzz", jos luku oli jaollinen kolmella sekä viidellä.
+#
+
+def fizz_buzz(numero):
+    return ""
+
+
+# 3. Tee ohjelma suuret_alkuun, joka käy luvut läpi 1-10. Jos luku on jaollinen kahdella se laitetaan listan alkuun,
+# muuten luku laitetaan listan loppuun.
+# Palauta lopuksi valmis lista.
+# Vinkki taululla löytyy funktiot .append ja .insert
+
+def suuret_alkuun():
+    taulu = []
+    return taulu
+
+
+# Tee funktio pisin_sana(sanat), joka palauttaa pisimman sanan sanat-taulukosta
+
+def pisin_sana(sanat):
+    return ""
+
+
+# Tee funktio pisimman_indeksi(sanat), joka palauttaa parin (a,b) (eli ns. tuplen), jossa a on pisin sana sanat-taulukosta ja
+# b on pisimman sanan indeksi
+def pisimman_indeksi(sanat):
+    return "", 0
+
+# selection sort?
